2704c2ac6360b565e1e6a9a0bdf8385322f11ea9
student <sebcheah@gmail.com>
2016-05-26 11:17:01 -0700

    Generate dump of floating point instructions
    Use make to clean/dump

---
+++ float-dump/float-test.c
@@ -0,0 +1,21 @@
+int main()
+{
+	/* code */
+	float a1 = 1.0;
+	float b1 = 1.5;
+	float c1 = a1 + b1;
+
+	float a2 = 2.1;
+	float b2 = 2.3;
+	float c2 = a2 - b2;
+
+	float a3 = 3.3;
+	float b3 = 3.7;
+	float c3 = a3 * b3;
+
+	float a4 = 4.2;
+	float b4 = 4.8;
+	float c4 = a4 / b4;
+	
+	return 0;
+}
