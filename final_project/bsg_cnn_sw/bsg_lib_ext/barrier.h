68d00227b265055563114e9f45054b5e86c11eb5
Yeseong Kim <myshepherd@gmail.com>
2016-06-03 11:48:48 -0700

    Comment fix in barrier

--- final_project/bsg_cnn_sw/bsg_lib_ext/barrier.h
+++ final_project/bsg_cnn_sw/bsg_lib_ext/barrier.h
@@ -1,4 +1,6 @@
-// Barrier library (software implementation without HW counts)
+// Barrier library:
+// Include both SW implementation and HW supported implemtation.
+// Each mode is selected by "USE_BROADCAST_IMPL". See barrier.c
 #ifndef _BARRIER_H_
 #define _BARRIER_H_
 
