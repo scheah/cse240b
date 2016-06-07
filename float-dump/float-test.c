0ef739fa138113798d58c15912b4c423f89f125f
Andrew Yang <ayang2007@gmail.com>
2016-06-07 09:46:47 -0700

    Adding softfloat libraries

--- float-dump/float-test.c
+++ float-dump/float-test.c
@@ -1,21 +1,61 @@
+
+#include "bsg_manycore.h"
+#include "softfloat.h"
+
+int bsg_x = -1;
+int bsg_y = -1;
+
+int bsg_set_tile_x_y()
+{
+  // everybody stores to tile 0,0
+  bsg_remote_store(0,0,&bsg_x,0);
+  bsg_remote_store(0,0,&bsg_y,0);
+
+  // make sure memory ops above are not moved down
+  bsg_compiler_memory_barrier();
+
+  // wait for my tile number to change
+  bsg_wait_while((bsg_volatile_access(bsg_x) == -1) || (bsg_volatile_access(bsg_y) == -1));
+
+  // make sure memory ops below are not moved above
+  bsg_compiler_memory_barrier();
+
+  // head of each column is responsible for
+  // propagating to next column
+  if ((bsg_x == 0)
+      && ((bsg_y + 1) != bsg_tiles_Y)
+    )
+  {
+    bsg_remote_store(0,bsg_y+1,&bsg_x,bsg_x);
+    bsg_remote_store(0,bsg_y+1,&bsg_y,bsg_y+1);
+  }
+
+  // propagate across each row
+  if ((bsg_x+1) != bsg_tiles_X)
+  {
+    bsg_remote_store(bsg_x+1,bsg_y,&bsg_x,bsg_x+1);
+    bsg_remote_store(bsg_x+1,bsg_y,&bsg_y,bsg_y);
+  }
+}
+
+int square(int c)
+{
+  return c*c;
+}
+
 int main()
 {
 	/* code */
-	float a1 = 1.0;
-	float b1 = 1.5;
-	float c1 = a1 + b1;
+  bsg_set_tile_x_y();
 
-	float a2 = 2.1;
-	float b2 = 2.3;
-	float c2 = a2 - b2;
+  bsg_remote_ptr_io_store(0,0x1260,bsg_x);
+  bsg_remote_ptr_io_store(0,0x1264,bsg_y);
 
-	float a3 = 3.3;
-	float b3 = 3.7;
-	float c3 = a3 * b3;
+  bsg_remote_ptr_io_store(0,0x1234,0x13);
 
-	float a4 = 4.2;
-	float b4 = 4.8;
-	float c4 = a4 / b4;
-	
-	return 0;
+  if ((bsg_x == bsg_tiles_X-1) && (bsg_y == bsg_tiles_Y-1))
+    bsg_finish();
+
+  bsg_wait_while(1);
+
 }
