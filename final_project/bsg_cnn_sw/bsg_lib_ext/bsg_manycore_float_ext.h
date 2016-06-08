f85acf9a6c4fba73a8a7168575cd296c9017b56d
Yeseong Kim <myshepherd@gmail.com>
2016-06-07 18:09:39 -0700

    floating point extention

--- final_project/bsg_cnn_sw/bsg_lib_ext/bsg_manycore_float_ext.h
+++ final_project/bsg_cnn_sw/bsg_lib_ext/bsg_manycore_float_ext.h
@@ -5,4 +5,13 @@
 
 typedef volatile float_tt   *bsg_remote_float_t_ptr;
 #define bsg_volatile_access_float(var)        (*((bsg_remote_float_t_ptr) (&(var))))
+#define bsg_remote_ptr_float(x,y,local_addr) ((bsg_remote_float_t_ptr) ( (1<<31)                                     \
+                                                               | ((y) << (31-(bsg_noc_ybits)))             \
+                                                               | ((x) << (31-bsg_noc_xbits-bsg_noc_ybits)) \
+                                                               | ((int) (local_addr))                      \
+                                                             )                                             \
+                                        )
+
+
+
 #endif
